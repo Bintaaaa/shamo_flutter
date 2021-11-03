89efbefe193de1c2f23861f639e7a634a785c617
Bintaaaa <bijantium.yt.32@gmail.com>
2021-11-03 22:30:54 +0700

    Initial commit

---
+++ lib/theme/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff6C5ECF);
+Color secondaryColor = const Color(0xff38ABBE);
+Color alertColor = const Color(0xffED6363);
+Color priceColor = const Color(0xff2C96F1);
+Color backgroundColor1 = const Color(0xff1F1D2B);
+Color backgroundColor2 = const Color(0xff2B2937);
+Color backgroundColor3 = const Color(0xff242231);
+Color primaryTextColor = const Color(0xffF1F0F2);
+Color secondaryTextColor = const Color(0x00ff9999);
